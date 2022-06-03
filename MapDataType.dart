90443cc5848c61d4e24217cac99f3842b3b2e913
dheeraj singh <dheeraj.singh_cs20@gla.ac.in>
2022-06-03 15:20:21 +0530

    dart-data

---
+++ MapDataType.dart
@@ -0,0 +1,26 @@
+//Map DataType (Dictionary in python)
+// void main()
+// {
+//   Map data= {
+//     "name":"Manish",
+//     "age":30,
+//     "adress":"Delhi",
+//     "PhoneNo":97887979
+//   };
+//   // print(data);
+//   print(data["adress"]);
+//   data["email"]="dheeral@gamil.com";
+//   print(data);
+//
+//   }
+ //I am interested i creating an object}
+void main()
+{  Map course=new Map();  //Map constructor
+course["name"]="Flutter";
+course["time"]="45 hours";
+course["fee"]=1000;
+print(course);
+print(course.runtimeType);
+}
+//Assignment
+// you have to create a list and print the entire list as a multiple of 5
---
+++ fluterassignment1.dart
@@ -0,0 +1,8 @@
+// you have to create a list and print the entire list as a multiple of 5
+void main() {
+  var l = [20, 10, 40, 55, 2, 1, 5, 6];
+  l.forEach((element) {
+    print(element * 5);
+  });
+}
+
