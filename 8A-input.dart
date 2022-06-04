c1057d1f0179a897b77d2ba1e0a9468742e08e6d
dheeraj singh <dheeraj.singh_cs20@gla.ac.in>
2022-06-04 13:25:02 +0530

    input to the user

--- 8A-input.dart
+++ 8A-input.dart
@@ -3,7 +3,11 @@ import 'dart:io';
 
 void main()
 {
-  print("enter your name: ");
-  String? name= stdin.readLineSync();
+  stdout.write("enter your name:"); //jus like print function
+  // print("enter your name: ");
+  String name= stdin.readLineSync()!;  //stdin(to take input from the user)
+  // String? name= stdin.readLineSync();
+  // null safety operator(?): user may enter null value
+   // null safety operator(!): user may not enter null value
   print(name);
 }
