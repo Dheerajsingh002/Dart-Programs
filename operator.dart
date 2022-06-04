f3845a40e8e2cf1a8f933543f02c23165f582d0c
dheeraj singh <dheeraj.singh_cs20@gla.ac.in>
2022-06-04 13:10:41 +0530

    all oiperators

---
+++ operator.dart
@@ -0,0 +1,77 @@
+void main() {
+  /*
+  Dart supports the following types of operators.
+  1. Arithmetic Operators
+  2. Assignment Operators
+  3. Relational Operators
+  4. Logical Operators
+  5. Conditional Operators
+  
+  */
+ var v1;
+ v1=30;
+ var v2=10;
+ var v3;
+ var v4;
+//  arthemetic operator
+//  print("v1+v2=${v1+v2}");
+//  print("v1+v2=${v1-v2}");
+//  print("v1+v2=${v1*v2}");
+//  print("v1+v2=${v1/v2}");
+//  print("v1+v2=${v1%v2}");
+// print("v1+v2=${v1~/v2}");
+
+//  unary operator
+// print(v1++);//postfix 
+// print(++v1);//prefix
+// print(--v1);//prefix
+// print(v1--); //postfix
+
+// v1=v3;
+// print(v1); //null
+
+
+
+// Assignment operator
+// v1+=10; 
+// v1-=10; 
+// v1*=10; 
+// v1/=10;  
+// v1%=10;
+// v1~/=10;
+// print(v1);
+
+
+
+// Relational operator
+// var res=v1<v2;
+// res=v1>v2;
+// res=v1>=v2;
+// res=v1<=v2;
+// res=v1==v2;
+// print("result is: ${res}");
+
+
+
+// Logical operator
+
+// bool x=true;
+// bool y=false;
+// var z=(v1<v2) && x; //'And' operator
+// var b=(v1<v2) || x;  //'Or' operator
+// var c=!(v1>v2);
+// print(z);
+// print(b);
+// print(c);
+
+
+
+//type operator
+int num=10;
+String name="pamkaj";
+bool isAdmin=true;
+var abc=20;
+print(num is int);
+print(name is int);
+print(isAdmin is bool);
+}
