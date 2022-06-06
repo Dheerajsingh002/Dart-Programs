7015db9673ae743324ae10dcd8ebffea14f1a7be
dheeraj singh <dheeraj.singh_cs20@gla.ac.in>
2022-06-06 14:48:56 +0530

    loop statements

---
+++ 13-loop.dart
@@ -0,0 +1,41 @@
+// loops are control structure used to itrate or repeate set of statements
+//1.for
+//2.While
+//3.do while
+//4.for in loop
+ 
+
+ void main()
+ {
+   // for loop
+  //  for(int i=0;i<10;i++)
+  //  {
+  //    print("loops $i");
+  //  }
+
+  // While loop
+  // int i=10;
+  // while(i>1)
+  // {
+  //  print("Loop-$i");
+  //  i--;
+  // }
+
+
+// do while loop
+// int i=5;
+// do{
+//   print("LOOP-$i");
+//   i+=5;
+// }while(i<=50);
+
+// for each loop
+List names=["manish","john","peter","Asha","Ankit"];
+for(var name in names)
+{
+  print(name);
+}
+ }
+
+
+ 
