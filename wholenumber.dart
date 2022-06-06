7244a169fc1fe1dd79c0ea15726f4c96e1537d34
dheeraj singh <dheeraj.singh_cs20@gla.ac.in>
2022-06-06 15:01:51 +0530

    wholenumber

--- wholenumber.dart
+++ wholenumber.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main()
 {
-  print("enter the range when be get a natural number: ");
+  print("enter the range when be get a whole number: ");
   int num=int.parse(stdin.readLineSync()!);
   for(int i=0;i<=num;i++)
   {
