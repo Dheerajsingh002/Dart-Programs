e57d454c325f446d34236d959ac279f589fb1861
dheeraj singh <dheeraj.singh_cs20@gla.ac.in>
2022-06-06 14:31:46 +0530

    ifelse statement

---
+++ 9-ifelse.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main()
+{
+  stdout.write("20+15: ");
+  int num=int.parse(stdin.readLineSync()!);
+  if(num==35)
+  {
+    print("correct result ");
+  }
+  else
+  {
+    print("Incorrect result ");
+  }
+
+}
