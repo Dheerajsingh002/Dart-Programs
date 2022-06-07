533351853caafb3516b3934d0aca52c7dc04b633
dheeraj singh <dheeraj.singh_cs20@gla.ac.in>
2022-06-07 14:41:16 +0530

    functions

---
+++ lesson3/1-functions.dart
@@ -0,0 +1,32 @@
+//Functions are of two types
+//1.system Defined FDunction
+//3.eg: main(),print(),write()
+//2.user defined | custom Function
+
+//functions having two parts
+//1.function defination: what are tasks performed by the function
+//preferly we put function defination to be outside of main()
+//2.Function calling:call the function that is already defined
+
+main()
+{
+  printSomething();
+}
+printSomething()
+{
+  print("this is user function");
+  print("you can call this function anywhere, anytime inside main function");
+  print(".............");
+}
+
+// There are various varient of a function
+//1. Function without input(no perameter) and without return
+//2.Function with some no Input and with return
+//3.function with no Input and with return
+//4.Function with some Input and with return
+
+
+//Category of function in new programming language
+//1.Named functiuon
+//2.Expression function
+//3.Anonymous function
