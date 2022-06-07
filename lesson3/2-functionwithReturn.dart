6569acefefde71ea178feabec7d83532df9ba55b
dheeraj singh <dheeraj.singh_cs20@gla.ac.in>
2022-06-07 14:41:43 +0530

    functionreturn

---
+++ lesson3/2-functionwithReturn.dart
@@ -0,0 +1,10 @@
+//2.Function with no Input and with return statement
+main()
+{
+  String result=printSomething();
+  print(result);
+}
+printSomething()
+{
+  return "this is a return message";
+}
