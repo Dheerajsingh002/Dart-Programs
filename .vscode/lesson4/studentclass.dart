ab0aae1f59f4fbf22eb115deadb25cae5eb505d9
dheeraj singh <dheeraj.singh_cs20@gla.ac.in>
2022-06-08 16:11:09 +0530

    Studentclass

---
+++ .vscode/lesson4/studentclass.dart
@@ -0,0 +1,20 @@
+class Student
+{
+  String? name;
+  String? Session;
+  int? rollno;
+  String? F_name;
+  String? M_name;
+  double? percentage;
+
+  Student(n,S,r,F,M,P)
+  {
+    this.name=n;
+    this.Session=S;
+    this.rollno=r;
+    this.F_name=F;
+    this.M_name=M;
+    this.percentage=P;
+  }
+
+}
