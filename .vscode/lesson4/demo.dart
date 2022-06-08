15d47023c1c984138c96c6838d9bdf526706d784
dheeraj singh <dheeraj.singh_cs20@gla.ac.in>
2022-06-08 14:57:13 +0530

    oops theory

---
+++ .vscode/lesson4/demo.dart
@@ -0,0 +1,29 @@
+//oop(object oriented language)
+//1.object Created Programming System  (html is a object creating programming)
+//2.object Based Programming System  (javascript)
+//3.object oriented Programming System (c++,python,java)
+// Difference between class and objects
+  //object is a real world identity  
+  //ojects have two things attribute and behaviour
+// class is just a blue print
+//class took memory also but negligible
+// car (class)
+    //.properties
+     //-model
+     //-color
+     //-seeting
+     //-Gears
+     //-Max speed
+    //.Behavour (instances of the class)
+     //-moveFordward
+     //-moveBackward
+//objects
+ //Hyundai
+ //Maruti
+ //Kia
+ 
+//Constructor
+  //constructor is used to initialize the data members
+  //its name should same as class name
+  //-it will called automatically
+  //-a special functiomn to create objects at the same initialize the object
